eb5790a33250937859327addf5fb4f006c9f9c93
Mirzoieva <107805547+Mirzoieva@users.noreply.github.com>
2022-06-25 00:08:34 +0300

    Add files via upload

---
+++ Lab_2_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Tariff> tariffs = new List<Tariff>()
+            {
+                new Tariff("1", 1000, 3000, 300),
+                new Tariff("2", 1500, 5000, 500),
+                new Tariff("3", 500, 2000, 150)
+            };
+
+            Tariff.tariffsHyerarchy(tariffs);
+            foreach (var item in tariffs)
+            {
+                item.NumOfClient();
+            }
+
+            Tariff.Sort(tariffs);
+            Tariff.InDiapasone(700, 1200, 100, 400, tariffs);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab_2_2/Tariff.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_2_2
+{
+    class Tariff
+    {
+        string name;
+        int traffic;
+        int numOfClient;
+        int cost;
+        public Tariff(string name, int traffic, int numOfClient, int cost)
+        {
+            this.name = name;
+            this.traffic = traffic;
+            this.numOfClient = numOfClient;
+            this.cost = cost;
+        }
+        public static void tariffsHyerarchy(List<Tariff> tariffs)
+        {
+            Console.WriteLine("Iєрархiя тарифiв: ");
+            Console.WriteLine("{0,10}   |{1,20}  |{2,20}  |{3,18} ", "Назва", "Трафiк", "Вартiсть", "Кiлькiсть клiєнтiв");
+            for (int i = 0; i < tariffs.Count; i++)
+            {
+                var t = tariffs[i];
+                Console.WriteLine("{0,10}   |{1,20}  |{2,20}  |{3,10} ", t.name, t.traffic, t.cost, t.numOfClient);
+            }
+        }
+
+        public void NumOfClient()
+        {
+            Console.WriteLine($"Тариф {name} має {numOfClient} клiєнтiв");
+        }
+
+        public static void Sort(List<Tariff> arr)
+        {
+            int len = arr.Count;
+            for (int i = 1; i < len; i++)
+            {
+                for (int j = 0; j < len - i; j++)
+                {
+                    if (arr[j].traffic > arr[j + 1].traffic)
+                    {
+                        Tariff temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Вiдсортованi тарифи згiдно об’єму трафiку:");
+            for (int i = 0; i < len; i++)
+            {
+                Console.WriteLine($"{arr[i].name} : {arr[i].traffic}");
+            }
+        }
+        public static void InDiapasone(int min1, int max1, int min2, int max2, List<Tariff> arr)
+        {
+            Console.WriteLine($"Тариф знаходиться в дiапазонi з {min1} до {max1} за об'ємом трафiку та з {min2} до {max2} за вартiстю абонплати:");
+            for (int i = 0; i < arr.Count; i++)
+            {
+                if (arr[i].traffic >= min1 && arr[i].traffic <= max1 && arr[i].cost >= min2 && arr[i].cost <= max2)
+                {
+                    Console.WriteLine($"{arr[i].name}: {arr[i].traffic} - трафiк, {arr[i].cost} - вартiсть");
+                }
+            }
+        }
+    }
+}
